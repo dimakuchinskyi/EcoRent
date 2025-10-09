795989f32e2d658bd168de82a192cd70f4d52c1c
Кучинський Дмитро Максимович <131793372+dimakuchinskyi@users.noreply.github.com>
2025-10-10 00:41:45 +0300

    Delete ElectricCarRental directory

--- ElectricCarRental/Feedback.cs
+++
@@ -1,9 +0,0 @@
-namespace ElectricCarRental;
-
-// Прості відгуки/звернення клієнтів замість SupportTicket
-public class Feedback
-{
-	public string FeedbackId { get; set; }
-	public string Message { get; set; }
-	public DateTime CreatedAt { get; set; } = DateTime.Now;
-} 
